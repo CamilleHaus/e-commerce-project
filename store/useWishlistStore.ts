39fd0de39875de6a4020e08f6b83261485500477
Camille Haus <camille_haus@hotmail.com>
2024-06-25 21:34:28 -0300

    Added useWishlistStore

---
+++ store/useWishlistStore.ts
@@ -0,0 +1,63 @@
+import { IProductType } from "@/types/productTypes";
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+
+type WishlistState = {
+  wishList: IProductType[];
+  addToWishlist: (item: IProductType) => void;
+  removeFromWishlist: (item: IProductType) => void;
+};
+
+export const useWishlistStore = create<WishlistState>()(
+  persist(
+    (set) => ({
+      // INITIAL VALUES
+      wishList: [],
+     
+
+      // SETTING THE STATE
+
+// Function to add an item to the wishlist
+addToWishlist: (item) =>
+  set((state) => {
+    // Check if the item is already in the wishlist
+    const existingItem = state.wishList.find((wishItem) => wishItem.id === item.id);
+
+    if (existingItem) {
+      // If the item is already in the wishlist, update its quantity to 1
+      const updatedWishList = state.wishList.map((wishItem) => {
+        if (wishItem.id === item.id) {
+          return { ...wishItem, quantity: 1 };
+        }
+        return wishItem;
+      });
+
+      // Return the updated wishlist
+      return { wishList: updatedWishList };
+    } else {
+      // If the item is not in the wishlist, add it with a quantity of 1
+      return { wishList: [...state.wishList, { ...item, quantity: 1 }] };
+    }
+  }),
+
+      removeFromWishlist: (item) =>
+        set((state) => {
+          const existingItem = state.wishList.find((wishItem) => wishItem.id === item.id);
+          if (existingItem && existingItem.quantity! > 1) {
+            const updatedWishList = state.wishList.map((wishItem) => {
+              if (wishItem.id === item.id) {
+                return { ...wishItem, quantity: wishItem.quantity! - 1 };
+              }
+              return wishItem;
+            });
+            return { wishList: updatedWishList };
+          } else {
+            const filteredWishList = state.wishList.filter((wishItem) => wishItem.id !== item.id);
+            return { wishList: filteredWishList };
+          }
+        }),
+    }),
+    { name: "wishlist-store" }
+  )
+);
